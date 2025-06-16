ade63595dbcdb740f591d9458184f1387c6eb380
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-06-16 21:01:04 +0800

    add uac support

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,26 @@
+const UAC: bool = false;
+
 fn main() {
-    tauri_build::build()
+    if UAC {
+        build_uac()
+    } else {
+        tauri_build::build();    
+    }    
+}
+
+fn build_uac(){
+    let mut windows = tauri_build::WindowsAttributes::new();
+    windows = windows.app_manifest(r#"
+    <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+      <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+          <security>
+              <requestedPrivileges>
+                  <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+              </requestedPrivileges>
+          </security>
+      </trustInfo>
+    </assembly>
+    "#);
+    let attrs =  tauri_build::Attributes::new().windows_attributes(windows);
+    tauri_build::try_build(attrs).expect("failed to run build script");
 }
