3773065959461f9946955af96a0bb188a38aad92
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-10-13 18:29:26 +0800

    fix online installer crash

--- src-tauri/src/app_service.rs
+++ src-tauri/src/app_service.rs
@@ -245,27 +245,29 @@ pub async fn load_apps() -> Result<Vec<App>, Error> {
 
             let mut needs_autostart = false;
             info!("locale is {}", get_locale());
-            if !is_latest {
-                info!("App is not the latest version.");
-                let app_name_clone = app.name.clone();
-                let latest_version = app.available_versions[0].clone();
-                if update_method == UPDATE_METHOD_OPTION_AUTO {
-                    info!("{}", t!("message.new_version_update", version=latest_version.clone()));
-                    send_notification(app_name_clone.clone(), t!("message.new_version_update", version=latest_version));
-                    update_to_version(&app_name_clone, &latest_version).await?;
-                    info!("Auto Update to version {} success.", &latest_version);
-                    send_notification(app_name_clone, t!("message.version_update_success", version=latest_version));
-                    needs_autostart = true;
-                } else {
-                    send_notification(app_name_clone.clone(), t!("message.new_version", version=latest_version));
-                    if update_method == UPDATE_METHOD_OPTION_IGNORE {
-                        info!("Auto-update is UPDATE_METHOD_OPTION_IGNORE set auto_start to true.");
+            if app.installed && !app.available_versions.is_empty() {
+                if !is_latest {
+                    info!("App is not the latest version.");
+                    let app_name_clone = app.name.clone();
+                    let latest_version = app.available_versions[0].clone();
+                    if update_method == UPDATE_METHOD_OPTION_AUTO {
+                        info!("{}", t!("message.new_version_update", version=latest_version.clone()));
+                        send_notification(app_name_clone.clone(), t!("message.new_version_update", version=latest_version));
+                        update_to_version(&app_name_clone, &latest_version).await?;
+                        info!("Auto Update to version {} success.", &latest_version);
+                        send_notification(app_name_clone, t!("message.version_update_success", version=latest_version));
                         needs_autostart = true;
+                    } else {
+                        send_notification(app_name_clone.clone(), t!("message.new_version", version=latest_version));
+                        if update_method == UPDATE_METHOD_OPTION_IGNORE {
+                            info!("Auto-update is UPDATE_METHOD_OPTION_IGNORE set auto_start to true.");
+                            needs_autostart = true;
+                        }
                     }
+                } else {
+                    needs_autostart = true;
+                    info!("App is the latest version and installed. set auto start to true");
                 }
-            } else if app.installed {
-                needs_autostart = true;
-                info!("App is the latest version and installed. set auto start to true");
             }
 
             if needs_autostart {
